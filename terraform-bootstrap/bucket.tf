4bdeb09e046cb2b40c90da48e68db801bd892b84
Kayle Verhiel <27997231+Kayle-Verhiel@users.noreply.github.com>
2026-03-09 22:31:08 +0100

    format bucket bootstrap

--- terraform-bootstrap/bucket.tf
+++ terraform-bootstrap/bucket.tf
@@ -11,9 +11,9 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "terraform_state_e
   bucket = aws_s3_bucket.tf_state.bucket
 
   rule {
-      apply_server_side_encryption_by_default {
-        sse_algorithm = "AES256"
-      }
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
   }
 }
 
