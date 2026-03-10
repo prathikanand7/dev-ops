28c8719d8f08bb7badf401b9769e259c2780c1d3
Kayle Verhiel <27997231+Kayle-Verhiel@users.noreply.github.com>
2026-03-10 02:11:47 +0100

    created extra tester files

---
+++ terraform/environments/batch-dev/get_job_status.py
@@ -0,0 +1,27 @@
+import requests
+import sys
+
+API_BASE_URL = "https://5xnkmozte8.execute-api.eu-west-1.amazonaws.com/dev"
+API_KEY = "<copy the api key from AWS>"
+
+if len(sys.argv) != 2:
+    print("Usage: python get_job_status.py <job_id>")
+    sys.exit(1)
+
+job_id = sys.argv[1]
+
+url = f"{API_BASE_URL}/batch/jobs/{job_id}"
+
+headers = {
+    "x-api-key": API_KEY
+}
+
+try:
+    response = requests.get(url, headers=headers)
+
+    print("\n=== JOB STATUS ===")
+    print(f"Status Code: {response.status_code}")
+    print(response.text)
+
+except Exception as e:
+    print(f"Request failed: {e}")
--- terraform/environments/batch-dev/test_live.py
+++ terraform/environments/batch-dev/test_live.py
@@ -1,13 +1,19 @@
 import requests
 
 # For quickly testing a lambda function you need to add a Function URL without IAM permissions using the AWS console to the lambda function.
-API_URL = "https://u4pufhbsi7badmhltqmqdnyvre0hfpmq.lambda-url.eu-west-1.on.aws/"
+API_URL = "https://5xnkmozte8.execute-api.eu-west-1.amazonaws.com/dev/batch/jobs"
+API_KEY = "<copy the api key from AWS>"
+
 
 NOTEBOOK_PATH = "../../../demo_input/Data_cleaning.ipynb"
 DATA_FILE_PATH = "../../../demo_input/Template_MBO_Example_raw_v3.xlsx"
 ENV_FILE_PATH = "../../../demo_input/environment.yaml"
 
 print(f"Sending {NOTEBOOK_PATH} and {DATA_FILE_PATH} to AWS...")
+
+headers = {
+    "x-api-key": API_KEY
+}
 
 with open(NOTEBOOK_PATH, 'rb') as nb_file, open(DATA_FILE_PATH, 'rb') as data_file, open(ENV_FILE_PATH, 'rb') as env_file:
     files = {
@@ -26,7 +32,7 @@ with open(NOTEBOOK_PATH, 'rb') as nb_file, open(DATA_FILE_PATH, 'rb') as data_fi
     }
 
     try:
-        response = requests.post(API_URL, files=files)
+        response = requests.post(API_URL, files=files, headers=headers)
         print("\n=== CLOUD RESPONSE ===")
         print(f"Status Code: {response.status_code}")
         print(response.text)
