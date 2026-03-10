d767b20748b83c7686b99494c6dcaae712c9af88
Eneko Retolaza <73554201+enekoreto@users.noreply.github.com>
2026-03-10 22:06:19 +0100

    Revert "refactor(terraform): split ec2 200gb resources into dedicated files"
    This reverts commit 9acb908608c39923a2e828f25e6233ae79fde366.

--- terraform/environments/batch-dev/batch_queue.tf
+++ terraform/environments/batch-dev/batch_queue.tf
@@ -19,3 +19,25 @@ resource "aws_batch_job_queue" "lifewatch_fargate_job_queue" {
     Name = "lifewatch-fargate-job-queue"
   }
 }
+
+resource "aws_batch_job_queue" "lifewatch_ec2_200gb_job_queue" {
+  name     = "lifewatch-ec2-200gb-job-queue"
+  state    = "ENABLED"
+  priority = 20
+
+  compute_environment_order {
+    order               = 1
+    compute_environment = aws_batch_compute_environment.lifewatch_ec2_200gb_environment.arn
+  }
+
+  job_state_time_limit_action {
+    state            = "RUNNABLE"
+    action           = "CANCEL"
+    max_time_seconds = 600
+    reason           = "MISCONFIGURATION:COMPUTE_ENVIRONMENT_MAX_RESOURCE"
+  }
+
+  tags = {
+    Name = "lifewatch-ec2-200gb-job-queue"
+  }
+}
